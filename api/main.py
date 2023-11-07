98079c4074a0b67c748921e240477446d659ec43
nahoon kang <ejrtks102020@gmail.com>
2023-11-07 13:56:46 +0900

    feat: add monitoring

--- api/main.py
+++ api/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import FileResponse
 from starlette.staticfiles import StaticFiles
 from router import question_router, answer_router, user_router
 import os
+from common.prometheus_util import *
+
 app = FastAPI()
 
 origins = [
@@ -24,6 +26,16 @@ app.include_router(question_router.router)
 app.include_router(answer_router.router)
 app.include_router(user_router.router)
 app.mount("/assets", StaticFiles(directory="../frontend/dist/assets"))
+
+@IN_PROGRESS.track_inprogress()
+@TIMINGS.time()
+@app.get('/metrics')
+def metrics():
+    REQUESTS.labels(method='GET', endpoint="/metrics", status_code=200).inc()
+    return Response(
+            media_type="text/plain",
+            content=generate_latest()
+            )
 
 @app.get("/")
 def index():
--- api/router/question_router.py
+++ api/router/question_router.py
@@ -7,32 +7,44 @@ import crud
 from typing import List
 from starlette import status
 from router.user_router import get_current_user
+from common.prometheus_util import *
 
 router = APIRouter(
     prefix="/api/question",
     tags=['question']
 )
 
+@TIMINGS.time()
+@IN_PROGRESS.track_inprogress()
 @router.get("/list", response_model=schema.question_schema.QuestionList)
 def question_list(db: Session=Depends(get_db), page: int = 0, size: int = 10, keyword: str = ''):
+    REQUESTS.labels(method='GET', endpoint='/api/question/list', status_code=200).inc()
     total, _question_list = crud.question_crud.get_question_list(db, skip=page*size, limit=size, keyword=keyword)
     return {
         'total': total,
         'question_list': _question_list
         }
 
+@TIMINGS.time()
+@IN_PROGRESS.track_inprogress()
 @router.get("/detail/{question_id}", response_model=schema.question_schema.Question)
 def question_detail(question_id: int, page: int = 0, size: int = 10, db: Session=Depends(get_db)):
+    REQUESTS.labels(method='GET', endpoint='/api/question/detail', status_code=200).inc()
     question = crud.question_crud.get_question(db=db, question_id=question_id)
     response = schema.question_schema.Question.from_orm(question)
     return response
 
+@TIMINGS.time()
+@IN_PROGRESS.track_inprogress()
 @router.post("/create", status_code=status.HTTP_204_NO_CONTENT)
 def question_create(_question_create: schema.question_schema.QuestionCreate,
                     db: Session = Depends(get_db),
                     current_user: User = Depends(get_current_user)):
+    REQUESTS.labels(method='POST', endpoint='/api/question/create', status_code=200).inc()
     crud.question_crud.create_question(db=db, question_create=_question_create, user=current_user)
 
+@TIMINGS.time()
+@IN_PROGRESS.track_inprogress()
 @router.put("/update", status_code=status.HTTP_204_NO_CONTENT)
 def question_update(_question_update: schema.question_schema.QuestionUpdate,
                     db: Session = Depends(get_db),
@@ -44,9 +56,11 @@ def question_update(_question_update: schema.question_schema.QuestionUpdate,
     if current_user.id != db_question.user.id:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="수정 권한이 없습니다.")
-    
     crud.question_crud.update_question(db=db, db_question=db_question, question_update=_question_update)
+    REQUESTS.labels(method='PUT', endpoint='/api/question/update', status_code=200).inc()
 
+@TIMINGS.time()
+@IN_PROGRESS.track_inprogress()
 @router.delete("/delete", status_code=status.HTTP_204_NO_CONTENT)
 def question_delete(_question_delete: schema.question_schema.QuestionDelete,
                     db: Session = Depends(get_db),
@@ -60,6 +74,7 @@ def question_delete(_question_delete: schema.question_schema.QuestionDelete,
                             detail="삭제 권한이 없습니다.")
     
     crud.question_crud.delete_question(db=db, db_question=db_question)
+    REQUESTS.labels(method='DELETE', endpoint='/api/question/delete', status_code=200).inc()
 
 @router.post("/vote", status_code=status.HTTP_204_NO_CONTENT)
 def question_vote(_question_vote: schema.question_schema.QuestionVote,
@@ -70,4 +85,4 @@ def question_vote(_question_vote: schema.question_schema.QuestionVote,
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="데이터를 찾을 수 없습니다.")
     crud.question_crud.vote_question(db=db, db_question=db_question, db_user=current_user)
-    
+
